c0f54c312c3e680c82ba25ae4f6c1e4746cd9c76
NMK <117380980+BohdanNMK@users.noreply.github.com>
2022-11-22 12:05:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 const arguments = process.argv
 
-function pointParking(name) {
+function getTextPointParking(name) {
   return `на стоянці "${name}":`;
 }
 
-function countFreeTaxi(count) {
+function getTextFreeTaxi(count) {
   return ` вільних авто: "${count}",`;
 }
 
-function numberTaxisGo(count) {
+function getTextTaxisArriving(count) {
   return ` авто що прибувають протягом 5 хвилин: "${count}",`;
 }
 
-function pointCarGo(count) {
+function getTextTaxiLeavePoint(count) {
   return ` авто що поїхали з точки: "${count}". `;
 }
 
-function associationAllParametr(nameParkingPoint, countFreeAuto, countTaxisGo, countLeavePoint) {
-  const name = pointParking(nameParkingPoint);
-  const free = countFreeTaxi(countFreeAuto);
-  const number = numberTaxisGo(countTaxisGo);
-  const point = pointCarGo(countLeavePoint);
+function associationAllParametr(nameParkingPoint, countFreeAuto, countTaxiArriving, countLeavePoint) {
+  const strName = getTextPointParking(nameParkingPoint);
+  const strFree = getTextFreeTaxi(countFreeAuto);
+  const strNumber = getTextTaxisArriving(countTaxiArriving);
+  const strLeave = getTextTaxiLeavePoint(countLeavePoint);
   const text = `Отримані данні показують що`;
 
   let strResultOrderOnTaxi = `То ж замовлення не може бути, опрацьованно.`;
   if (countFreeAuto > 0) {
     strResultOrderOnTaxi = `То ж замовлення може бути, опрацьованно.`;
-  } else if (countFreeAuto === 0 && countTaxisGo > 0) {
+  } else if (countFreeAuto === 0 && countTaxiArriving > 0) {
     strResultOrderOnTaxi = `То ж замовлення може бути, опрацьованно протягом 5 хвилин.`;
   }
-  return `${text} ${name}${free}${number}${point}${strResultOrderOnTaxi}`
+  return `${text} ${strName}${strFree}${strNumber}${strLeave}${strResultOrderOnTaxi}`
 }
 
 console.log(associationAllParametr(arguments[2], parseInt(arguments[3]), parseInt(arguments[4]), parseInt(arguments[5])));
