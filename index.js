ede067ffd4b6c21bcb55f3c9a25a44e4f29d9348
NMK <117380980+BohdanNMK@users.noreply.github.com>
2022-11-22 11:32:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
 const arguments = process.argv
-function pointParking (nameParkingPoint) {
-  return `на стоянці "${nameParkingPoint}":`;
-}
-function quantityTaxi (freeAuto) {
-   return ` вільних авто: "${freeAuto}",`;
- }
- function numberTaxisGo (TaxisGo) {
-   return ` авто що прибувають протягом 5 хвилин: "${TaxisGo}",`;
- }
- function pointCarGo (GoOutPoint) {
-   return ` авто що поїхали з точки: "${GoOutPoint}". `;
- }
-function associationAllparametr(nameParkingPoint,freeAuto,TaxisGo,GoOutPoint) {
-const name = pointParking (nameParkingPoint);
-const free = quantityTaxi (freeAuto);
-const number = numberTaxisGo (TaxisGo);
-const point = pointCarGo (GoOutPoint);
-const text = `Отримані данні показують що`;
-let strResultOrderOnTaxi = `То ж замовлення не може бути, опрацьованно.`;
-    if (freeAuto>0) {
-   strResultOrderOnTaxi =`То ж замовлення може бути, опрацьованно.`;
-}
-    else if (freeAuto===0 && TaxisGo > 0) {
-   strResultOrderOnTaxi =`То ж замовлення може бути, опрацьованно протягом 5 хвилин.`;
-}
-  
-    return `${text} ${name}${free}${number}${point}${strResultOrderOnTaxi}`
 
+function pointParking(name) {
+  return `на стоянці "${name}":`;
 }
 
-console.log(associationAllparametr(arguments[2],parseInt(arguments[3]),parseInt(arguments[4]),parseInt(arguments[5])));
+function countFreeTaxi(count) {
+  return ` вільних авто: "${count}",`;
+}
+
+function numberTaxisGo(count) {
+  return ` авто що прибувають протягом 5 хвилин: "${count}",`;
+}
+
+function pointCarGo(count) {
+  return ` авто що поїхали з точки: "${count}". `;
+}
+
+function associationAllParametr(nameParkingPoint, countFreeAuto, countTaxisGo, countLeavePoint) {
+  const name = pointParking(nameParkingPoint);
+  const free = countFreeTaxi(countFreeAuto);
+  const number = numberTaxisGo(countTaxisGo);
+  const point = pointCarGo(countLeavePoint);
+  const text = `Отримані данні показують що`;
+
+  let strResultOrderOnTaxi = `То ж замовлення не може бути, опрацьованно.`;
+  if (countFreeAuto > 0) {
+    strResultOrderOnTaxi = `То ж замовлення може бути, опрацьованно.`;
+  } else if (countFreeAuto === 0 && countTaxisGo > 0) {
+    strResultOrderOnTaxi = `То ж замовлення може бути, опрацьованно протягом 5 хвилин.`;
+  }
+  return `${text} ${name}${free}${number}${point}${strResultOrderOnTaxi}`
+}
+
+console.log(associationAllParametr(arguments[2], parseInt(arguments[3]), parseInt(arguments[4]), parseInt(arguments[5])));
